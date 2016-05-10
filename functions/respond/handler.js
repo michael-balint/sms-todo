1852692a4a3a02ba9257ee308e1fff7d4c7ba00a
Andrew Stroup <andrew.m.stroup@gmail.com>
2016-05-09 23:15:58 -0400

    added toString due to GET

--- functions/respond/handler.js
+++ functions/respond/handler.js
@@ -14,7 +14,7 @@ let db = new AWS.DynamoDB.DocumentClient();
 
 module.exports.handler = (event, context, callback) => {
 
-  if (event.To !== config.PHONE) {
+  if (event.To.toString() !== config.PHONE) {
     console.log(event, config);
     return callback(new Error("Invalid input."));
   }
