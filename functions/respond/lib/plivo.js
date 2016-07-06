bf45ffd8c1067c28bd4c50739d51222fc99ed9f4
Michael Balint <michael@balint.us>
2016-07-06 17:37:20 -0400

    Added message validation (so ppl can't use our endpoint without a real text).

--- functions/respond/lib/plivo.js
+++ functions/respond/lib/plivo.js
@@ -5,9 +5,59 @@ var Plivo = require('plivo');
 var plivoCreds = require('../plivo-creds.json');
 var plivo = Plivo.RestAPI(plivoCreds);
 
+// returns an error if invalid
+function validateMessage(params, callback) {
+  // params = {
+  //   To
+  //   From
+  //   Text
+  //   Type
+  //   MessageUUID
+  // }
+
+  // skip validation if we are in offline mode
+  // if (process.env.IS_OFFLINE) {
+  //   return callback();
+  // }
+  if (!params.MessageUUID) {
+    return callback(new Error(`Invalid 'uuid' field in message.`));
+  }
+  if (!params.From) {
+    return callback(new Error(`Invalid 'from' field in message.`));
+  }
+  if (!params.To) {
+    return callback(new Error(`Invalid 'to' field in message.`));
+  }
+  plivo.get_message({record_id: params.MessageUUID}, (status, response) => {
+    if (status !== 202 && status !== 200) {
+      return callback(new Error(
+        `Plivo error when validating message: ${JSON.stringify(response)}`
+      ));
+    }
+
+    const {
+      message_state, message_direction, message_type, from_number, to_number
+    } = response;
+
+    if (message_state !== "delivered" ||
+        message_direction !== "inbound" ||
+        message_type !== "sms"
+        // from_number !== params.From ||
+        // to_number !== params.To
+    ) {
+      return callback(new Error(
+        `Plivo mismatch when validating message: ${JSON.stringify(response)}`
+      ));
+    }
+
+    // successfully validated
+    return callback();
+  });
+}
+
 function sendMessage(params, callback) {
   plivo.send_message(params, (status, response) => {
-    if (status != 202 && status != 200) {
+    if (status !== 202 && status !== 200) {
       console.error("Plivo error:", status);
     }
     return callback(null, JSON.stringify(response, null, 2));
@@ -15,5 +65,6 @@ function sendMessage(params, callback) {
 }
 
 module.exports = {
-  sendMessage: sendMessage
+  validateMessage,
+  sendMessage
 }
