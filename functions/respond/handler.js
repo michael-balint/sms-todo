464f532a7673817a5f91d57ffda975469fefaa03
Michael Balint <michael.balint@pif.gov>
2016-07-22 18:26:35 -0400

    Fixed verification.

--- functions/respond/handler.js
+++ functions/respond/handler.js
@@ -5,12 +5,12 @@ const twilio = require('./lib/twilio.js');
 
 // Serverless function (GET)
 module.exports.handler = (event, context, callback) => {
-  var params = event.body;
+  console.log(`Received message: ${JSON.stringify(event)}`);
 
   // TODO: add twilio.validateMessage call and refactor the following into submethods...
-  twilio.validateMessage(params, (err, response) => {
+  twilio.validateMessage(event, (err, response) => {
     if (response.status === "Valid") {
-      return sms.handleMessage(params, callback);
+      return sms.handleMessage(event, callback);
     } else {
       return callback(new Error("Message must be sent from a mobile device."));
     }
--- functions/respond/lib/twilio.js
+++ functions/respond/lib/twilio.js
@@ -5,9 +5,55 @@ var twilioCreds = require("../twilio-creds.json");
 var twilio = require('twilio');
 var client = twilio(twilioCreds.account_sid, twilioCreds.auth_token);
 
-// returns (err, response) - where response.status === "Valid" if valid
+// returns an error if invalid
 function validateMessage(params, callback) {
-  return callback(null, { status: "Valid" });
+  // params = {
+  //   To
+  //   From
+  //   Text
+  //   Type
+  //   MessageUUID
+  // }
+
+  // skip validation if we are in offline mode
+  // if (process.env.IS_OFFLINE) {
+  //   return callback();
+  // }
+  if (!params.MessageSid) {
+    return callback(new Error(`Invalid 'MessageSid' field.`), {"status": "Invalid"});
+  }
+  if (!params.From) {
+    return callback(new Error(`Invalid 'From' field.`), {"status": "Invalid"});
+  }
+  if (!params.To) {
+    return callback(new Error(`Invalid 'To' field.`), {"status": "Invalid"});
+  }
+  console.log(`Verifying message with sid ${params.MessageSid}.`);
+  client.getMessage(params.MessageSid, (err, response) => {
+    if (err) {
+      return callback(new Error(
+        `Error when validating message: ${JSON.stringify(err)}.`
+      ), {"status": "Invalid"});
+    }
+
+    const sid = response.sid;
+    const direction = response.direction;
+    const message_type = response.message_type;
+    const from_number = response.from_number;
+    const to_number = response.to_number;
+
+    if (
+      sid !== params.MessageSid ||
+      direction !== "inbound"
+    ) {
+      return callback(new Error(
+        `Mismatch when validating message: ${JSON.stringify(response)}`
+      ));
+    }
+
+    // successfully validated
+    return callback(null, {"status": "Valid"});
+  });
 }
 
 function sendMessage(params, callback) {
